9c90a439ba1ad340aeb9b8a30c175cee63336db2
Serafim <guilhermeserafimbonfim@hotmail.com>
2022-03-30 18:41:12 -0300

    feat(index.js): add sales and products routers

--- index.js
+++ index.js
@@ -1,9 +1,20 @@
+const express = require('express');
+
 require('dotenv').config();
+
+const salesRouter = require('./routers/sales');
+const productsRouter = require('./routers/products');
+
+const app = express();
+app.use(express.json());
 
 // não remova esse endpoint, e para o avaliador funcionar
 app.get('/', (_request, response) => {
   response.send();
 });
+
+app.use(salesRouter);
+app.use(productsRouter);
 
 app.listen(process.env.PORT, () => {
   console.log(`Escutando na porta ${process.env.PORT}`);
