a26d5c5db54e96f722cec78ca1cde1bce0c5e067
Serafim <guilhermeserafimbonfim@hotmail.com>
2022-03-30 22:21:43 -0300

    fix(models/connection): change connection

--- models/connection.js
+++ models/connection.js
@@ -1,10 +1,9 @@
 const mysql = require('mysql2/promise');
 
 const connection = mysql.createPool({
-    host: 'localhost',
-    user: 'trybe',
-    password: 'Trybe123*',
-    database: 'StoreManager',
-    port: 3306 });
+  host: process.env.MYSQL_HOST,
+  user: process.env.MYSQL_USER,
+  password: process.env.MYSQL_PASSWORD,
+});
 
 module.exports = connection;
