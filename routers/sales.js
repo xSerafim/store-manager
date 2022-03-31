0ecdfff5c03851ac1bec3118f894b97cf14aded5
Serafim <guilhermeserafimbonfim@hotmail.com>
2022-03-30 22:25:33 -0300

    feat(routers/sales): add controllers to router

--- routers/sales.js
+++ routers/sales.js
@@ -1,9 +1,14 @@
 const express = require('express');
 
+const {
+  allSales,
+  saleById,
+} = require('../controllers/sales');
+
 const router = express.Router();
 
 router
-  .get('/sales')
-  .get('/sales/:id');
+  .get('/sales', allSales)
+  .get('/sales/:id', saleById);
 
 module.exports = router;
