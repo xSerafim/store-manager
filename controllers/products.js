f52c8a8e2151e05d826637adb16fea536fe5fc52
Serafim <guilhermeserafimbonfim@hotmail.com>
2022-04-02 09:31:05 -0300

    feat: add new functions - change functions names - add function to deal with put request - add function to deal with delete request

--- controllers/products.js
+++ controllers/products.js
@@ -1,11 +1,14 @@
 const {
-  getAllProducts,
-  getProductById,
+  allProducts,
+  productById,
   createProduct,
+  updateProduct,
+  deleteProduct,
 } = require('../services/products');
 
 const OK_STATUS = 200;
 const CREATED_STATUS = 201;
+const NO_CONTENT = 204;
 const NOT_FOUND_STATUS = 404;
 const CONFLICT_STATUS = 409;
 const SERVER_ERROR_STATUS = 500;
@@ -15,19 +18,19 @@ function serverError(res, error) {
   return res.status(SERVER_ERROR_STATUS).json({ message: 'Erro no servidor' });
 }
 
-async function allProducts(_req, res) {
+async function getAllProducts(_req, res) {
   try {
-    const products = await getAllProducts();
+    const products = await allProducts();
       return res.status(OK_STATUS).json(products);
   } catch (error) {
     return serverError(res, error);
   }
 }
 
-async function productById(req, res) {
+async function getProductById(req, res) {
   try {
     const { id } = req.params;
-    const product = await getProductById(id);
+    const product = await productById(id);
     if (!product[0]) return res.status(NOT_FOUND_STATUS).json({ message: 'Product not found' });
   
     return res.status(OK_STATUS).json(...product);
@@ -36,7 +39,7 @@ async function productById(req, res) {
   }
 }
 
-async function newProduct(req, res) {
+async function postProduct(req, res) {
   try {
     const product = req.body;
     const createdProduct = await createProduct(product);
@@ -49,8 +52,35 @@ async function newProduct(req, res) {
   }
 }
 
+async function putProduct(req, res) {
+  try {
+    const { id } = req.params;
+    const { name, quantity } = req.body;
+    const product = await updateProduct(id, name, quantity);
+
+    if (product) return res.status(OK_STATUS).json(product);
+    return res.status(NOT_FOUND_STATUS).json({ message: 'Product not found' });
+  } catch (error) {
+    return serverError(res, error);
+  }
+}
+
+async function delProduct(req, res) {
+  try {
+    const { id } = req.params;
+    const product = await deleteProduct(id);
+
+    if (product) return res.status(NO_CONTENT).end();
+    return res.status(NOT_FOUND_STATUS).json({ message: 'Product not found' });
+  } catch (error) {
+    return serverError(res, error);
+  }
+}
+
 module.exports = {
-  allProducts,
-  productById,
-  newProduct,
+  getAllProducts,
+  getProductById,
+  postProduct,
+  putProduct,
+  delProduct,
 };
