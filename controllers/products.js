e619cf3d061d5de2de24c4fa9a70db9e8fd47d70
Serafim <guilhermeserafimbonfim@hotmail.com>
2022-04-02 08:13:39 -0300

    feat: add new products controllers - function to deal with server error message - function to deal with post request

--- controllers/products.js
+++ controllers/products.js
@@ -1,19 +1,26 @@
 const {
   getAllProducts,
   getProductById,
+  createProduct,
 } = require('../services/products');
 
-const HTTP_OK_STATUS = 200;
-const NOT_FOUND = 404;
-const SERVER_ERROR = 500;
+const OK_STATUS = 200;
+const CREATED_STATUS = 201;
+const NOT_FOUND_STATUS = 404;
+const CONFLICT_STATUS = 409;
+const SERVER_ERROR_STATUS = 500;
+
+function serverError(res, error) {
+  console.log(error);
+  return res.status(SERVER_ERROR_STATUS).json({ message: 'Erro no servidor' });
+}
 
 async function allProducts(_req, res) {
   try {
     const products = await getAllProducts();
-      return res.status(HTTP_OK_STATUS).json(products);
+      return res.status(OK_STATUS).json(products);
   } catch (error) {
-    console.log(error);
-    return res.status(SERVER_ERROR).json({ message: 'Erro no servidor' });
+    return serverError(res, error);
   }
 }
 
@@ -21,15 +28,29 @@ async function productById(req, res) {
   try {
     const { id } = req.params;
     const product = await getProductById(id);
-    if (!product[0]) return res.status(NOT_FOUND).json({ message: 'Product not found' });
+    if (!product[0]) return res.status(NOT_FOUND_STATUS).json({ message: 'Product not found' });
   
-    return res.status(HTTP_OK_STATUS).json(...product);
+    return res.status(OK_STATUS).json(...product);
   } catch (error) {
-    console.log(error);
+    return serverError(res, error);
+  }
+}
+
+async function newProduct(req, res) {
+  try {
+    const product = req.body;
+    const createdProduct = await createProduct(product);
+    if (createdProduct) {
+      return res.status(CREATED_STATUS).json(createdProduct);
+    }
+    return res.status(CONFLICT_STATUS).json({ message: 'Product already exists' });
+  } catch (error) {
+    return serverError(res, error);
   }
 }
 
 module.exports = {
   allProducts,
   productById,
+  newProduct,
 };
