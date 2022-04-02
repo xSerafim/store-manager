97b7c40740adbad85dc795852c333af727e2c8f5
Serafim <guilhermeserafimbonfim@hotmail.com>
2022-04-02 08:15:47 -0300

    feat: add new sales controllers - function to deal with server error message

--- controllers/sales.js
+++ controllers/sales.js
@@ -7,13 +7,17 @@ const HTTP_OK_STATUS = 200;
 const SERVER_ERROR = 500;
 const NOT_FOUND = 404;
 
+function serverError(res, error) {
+  console.log(error);
+  return res.status(SERVER_ERROR).json({ message: 'Erro no servidor' });
+}
+
 async function allSales(_req, res) {
   try {
     const sales = await getAllSales();
       return res.status(HTTP_OK_STATUS).json(sales);
   } catch (error) {
-    console.log(error);
-    return res.status(SERVER_ERROR).json({ message: 'Erro no servidor' });
+    return serverError(res, error);
   }
 }
 
@@ -27,7 +31,7 @@ async function saleById(req, res) {
     sale.forEach((e) => delete e.saleId);
     return res.status(HTTP_OK_STATUS).json(sale);
   } catch (error) {
-    console.log(error);
+    return serverError(res, error);
   }
 }
 
