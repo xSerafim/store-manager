6e18b801e4fed3ee3472e1db9bd9ef3eff062c50
Serafim <guilhermeserafimbonfim@hotmail.com>
2022-04-04 18:54:43 -0300

    feat: add validateProduct to routes

--- routers/sales.js
+++ routers/sales.js
@@ -4,13 +4,19 @@ const {
   allSales,
   saleById,
   registerNewSale,
+  updtSale,
 } = require('../controllers/sales');
+
+const { validateProduct } = require('../middlewares/salesMiddlewares');
 
 const router = express.Router();
 
 router
   .get('/', allSales)
   .get('/:id', saleById)
-  .post('/', registerNewSale);
+  .post('/', validateProduct,
+    registerNewSale)
+  .put('/:id', validateProduct,
+    updtSale);
 
 module.exports = router;
