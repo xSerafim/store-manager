826019a4b1b4dca930b370759daec47298080810
Serafim <guilhermeserafimbonfim@hotmail.com>
2022-04-04 15:58:21 -0300

    feat: add post router

--- routers/sales.js
+++ routers/sales.js
@@ -3,12 +3,14 @@ const express = require('express');
 const {
   allSales,
   saleById,
+  registerNewSale,
 } = require('../controllers/sales');
 
 const router = express.Router();
 
 router
   .get('/', allSales)
-  .get('/:id', saleById);
+  .get('/:id', saleById)
+  .post('/', registerNewSale);
 
 module.exports = router;
