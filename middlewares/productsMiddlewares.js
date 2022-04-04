e676b35ca0d86301078d02d4a9dae7088dee605d
Serafim <guilhermeserafimbonfim@hotmail.com>
2022-04-04 15:56:51 -0300

    fix: change string required to any

--- middlewares/productsMiddlewares.js
+++ middlewares/productsMiddlewares.js
@@ -4,12 +4,12 @@ const productSchema = JOI.object({
   name: JOI.string().min(5).required().messages({
     'string.base': '400*"name" must be a string',
     'string.min': '422*"name" length must be at least 5 characters long',
-    'string.required': '400*"name" is required',
+    'any.required': '400*"name" is required',
   }),
   quantity: JOI.number().min(1).required().messages({
     'number.base': '400*"quantity" must be a string',
     'number.min': '422*"quantity" must be greater than or equal to 1',
-    'number.required': '400*"quantity" is required',
+    'any.required': '400*"quantity" is required',
   }),
 });
 
