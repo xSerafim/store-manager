8d0319da7e0bd42b0565e43822996adc9cd70bdf
Serafim <guilhermeserafimbonfim@hotmail.com>
2022-04-05 20:43:22 -0300

    feat(mocks/salesData): add sales mock

---
+++ test/unit/mocks/salesData.js
@@ -0,0 +1,58 @@
+const allSales = [
+  {
+    saleId: 1,
+    date: '2022-04-05T13:50:33.000Z',
+    productId: 1,
+    quantity: 5
+  },
+  {
+    saleId: 1,
+    date: '2022-04-05T13:50:33.000Z',
+    productId: 2,
+    quantity: 10
+  },
+  {
+    saleId: 2,
+    date: '2022-04-05T13:50:33.000Z',
+    productId: 3,
+    quantity: 15
+  },
+  {
+    saleId: 4,
+    date: '2022-04-05T23:22:43.000Z',
+    productId: 1,
+    quantity: 5
+  },
+  {
+    saleId: 4,
+    date: '2022-04-05T23:22:43.000Z',
+    productId: 3,
+    quantity: 12
+  }
+];
+
+const sale = [
+  {
+    saleId: 1,
+    date: '2022-04-05T13:50:33.000Z',
+    productId: 1,
+    quantity: 5
+  },
+  {
+    saleId: 1,
+    date: '2022-04-05T13:50:33.000Z',
+    productId: 2,
+    quantity: 10
+  }
+];
+
+const insertId = [{ insertId: 4 }];
+
+const newSale = [{ productId: 1, quantity: 5 }, { productId: 3, quantity: 12 }];
+
+module.exports = {
+  allSales,
+  sale,
+  insertId,
+  newSale,
+};
