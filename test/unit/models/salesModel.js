f5265dee0dd6f6b40e6982eefa09d091dd275bee
Serafim <guilhermeserafimbonfim@hotmail.com>
2022-04-06 08:46:10 -0300

    feat(unit/models):add update and delete sale tests

--- test/unit/models/salesModel.js
+++ test/unit/models/salesModel.js
@@ -68,4 +68,32 @@ describe('Testa salesModel', () => {
       expect(connection.execute.called).to.be.true;
     });
   });
+  describe('Testa função updateSale', () => {
+    before(() => {
+      sinon.stub(connection, "execute");
+    });
+    after(() => {
+      connection.execute.restore();
+    });
+    it('Retorna um array de objetos igual o esperado', async () => {
+      const newSale = await model.updateSale(4, mocks.newSale);
+      expect(newSale).to.be.deep.equal({ saleId: 4, itemUpdated: mocks.newSale });
+    });
+    it('Connection é chamado', async () => {
+      await model.updateSale(4, mocks.newSale);
+      expect(connection.execute.called).to.be.true;
+    });
+  });
+  describe('Testa se a função deleteSale', () => {
+    before(() => {
+      sinon.stub(connection, "execute");
+    });
+    after(() => {
+      connection.execute.restore();
+    });
+    it('Chama o connection.execute com o argumento correto', async () => {
+      await model.deleteSale(1);
+      expect(connection.execute.calledWith('DELETE FROM sales WHERE id = ?;', [1])).to.be.true;
+    });
+  });
 });
